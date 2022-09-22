1ace430364942003dc7f5872128061316506faa6
Tabitha <spencersalmon.tabitha@gmail.com>
2022-09-22 19:25:32 -0400

    Updates

---
+++ README.md
@@ -0,0 +1,38 @@
+
+# React Portfolio
+    
+## Description
+    
+WORK IN PROGRESS. The purpose of this project is to make a professional portfolio using React that will eventually be what I will show employers. So far, I have completed all but the form validations on the Contact Me page.
+    
+## Table of Contents
+
+[Description](#description)
+
+[Link](#link)
+
+[Shout-outs](#shout-outs)
+
+[License](#license)
+
+[Questions](#questions)
+
+
+
+## Link
+
+
+    
+## Shout-Outs
+    
+Thanks to Leif Hetland and Ben White for all their help in office hours. 
+    
+## License 
+
+[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)
+    
+## Questions?
+
+
+
+[https://github.com/TabithaLy](https://github.com/TabithaLy)
---
+++ react-portfolio/src/components/pages/Contact.js
@@ -0,0 +1,74 @@
+import React, { useState } from 'react';
+import '../utils/main.css';
+import { validateEmail } from '../utils/helpers';
+
+export default function Contact() {
+
+  const [email, setEmail] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    if (!validateEmail(email)) {
+      setErrorMessage('Email or username is invalid');
+      return;
+    }
+    setEmail('');
+  };
+
+  const handleInputChange = (e) => {
+    // Getting the value and name of the input which triggered the change
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
+
+    // Based on the input type, we set the state of either email, username, and password
+    if (inputType === 'email') {
+      setEmail(inputValue);
+    }
+  };
+
+
+  return (
+    <>
+    <form className="page">
+      <h2>Contact Me</h2>
+      <div className="space">
+        <label>Email</label>
+        <div>
+          <input 
+            value={email} 
+            type="email" 
+            onChange={handleInputChange} 
+            placeholder="e.g. alex@example.com"
+            />
+        </div>
+      </div>
+
+      <div className="space">
+        <label>Name</label>
+        <div>
+          <input type="text"></input>
+        </div>
+      </div>
+
+      <div className="space">
+        <label>Message</label>
+        <div>
+          <textarea cols="40" rows="10" type="text" placeholder="Message"></textarea>
+        </div>
+      </div>
+
+      <div>
+        <button className='btn btn-dark btn-outline-light' type="button" onClick={ handleFormSubmit }>Submit</button>
+      </div>
+
+    </form>
+    {errorMessage && (
+      <div>
+        <p className="error-text">{errorMessage}</p>
+      </div>
+    )}
+    </>
+  );
+}
